a12a6aa0cda97b004ead02c64b0a21ca337053e2
med363 <blibechmedamine@gmail.com>
2023-05-28 18:01:42 +0100

    update compose

--- Vagrant/ansible.sh
+++ Vagrant/ansible.sh
@@ -10,6 +10,12 @@ sudo -H pip3 install --upgrade pip
 # Install Ansible
 sudo -H pip install ansible
 
+# check inventory playbook
+echo -e "\nRUNNING ANSISIBLE [LIST OF HOSTS] **************************************************\n"
+ansible-inventory -i hosts --list
+echo -e "\nRunning ANSIBLE [PING FOR ALL HOSTS] ***********************************************\n"
+ansible all -i hosts -m ping
+
 # Run Ansible playbooks
 echo -e "\nRUNNING ANSIBLE [cluster.yml] **************************************************\n"
 ansible-playbook -i hosts cluster.yml
